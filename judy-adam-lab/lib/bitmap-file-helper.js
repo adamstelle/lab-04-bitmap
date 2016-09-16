f0451ba44423e2a73a457c9368901411dcec83d6
JudyVue <judyvue87@gmail.com>
2016-09-16 13:03:16 -0700

    took out callback5

--- judy-adam-lab/lib/bitmap-file-helper.js
+++ judy-adam-lab/lib/bitmap-file-helper.js
@@ -6,14 +6,13 @@ const fs = require('fs');
 
 module.exports = exports = {};
 
-exports.readPicData = function(filepath, callback1, callback2, callback3, callback4, callback5){
+exports.readPicData = function(filepath, callback1, callback2, callback3, callback4){
   fs.readFile(filepath, (err, data) => {
     if (err) exports.errorHandler(err);
     var picData = callback1(data);
     callback2(data,picData);
     callback3(data,picData);
     callback4(data,picData);
-    callback5(data,picData);
   });
 };
 
