704259ac020d0fd95f17f5f8d49f95037a3e06a1
Adam Stelle <adamstelle@gmail.com>
2016-09-15 17:18:03 -0700

    Working bitmap color testing to identify RGB sequence

--- judy-adam-lab/model/color-experiment.js
+++ judy-adam-lab/model/color-experiment.js
@@ -1,9 +1,25 @@
 'use strict';
 
+const fs = require('fs');
+
 module.exports = function colorExperiment(buffer, bitMapHeader) {
   var colorArrayStart = 54;
   var colorArrayEnd = bitMapHeader.offset;
-  var colorArray = buffer.readUInt8(1);
-  console.log(buffer);
-  console.log(colorArray);
+  var colorArray = [];
+  for (var i=colorArrayStart; i < colorArrayEnd; i++) {
+    colorArray.push(buffer.readUInt8(i));
+  }
+  var modifiedColorArray = buffer.slice(54,colorArrayEnd);
+  for (var j=3; j<modifiedColorArray.length; j++) {
+    modifiedColorArray[j] = 255;
+    j++;
+    modifiedColorArray[j] = 0;
+    j++;
+    modifiedColorArray[j] = 0;
+    j++;
+    modifiedColorArray[j] = 0;
+  }
+  fs.writeFile(`${__dirname}/test${i}.bmp`, buffer, (err) => {
+    if(err) throw err;
+  });
 };
