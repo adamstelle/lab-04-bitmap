eed3ab42c45c3fa2ab12eff5c82eee3e93c2907a
Adam Stelle <adamstelle@gmail.com>
2016-09-16 17:44:40 -0700

    Refactored color manipulations module

--- judy-adam-lab/model/color-constructor.js
+++ judy-adam-lab/model/color-constructor.js
@@ -1,24 +1,40 @@
 'use strict';
 
 const fs = require('fs');
-module.exports = exports = {};
+const errorHandler = require(`${__dirname}/error-handler.js`);
 
-exports.colorInvert = function (buffer, picData) {
-  var colorArrayEnd = picData.offset;
-  var colorArray = buffer.slice(54,colorArrayEnd);
-  for (var i=0; i<colorArrayEnd; i++) {
-    colorArray[i] = 255 - colorArray[i];
+const runManipulations = module.exports = function(buffer, picData) {
+  colorInvert(buffer, picData);
+  greyScale(buffer, picData);
+  // rgbScale(buffer, picData); 
+};
+
+var ColorManipulation = function(buffer, picData) {
+  ColorManipulation.colorArrayEnd = picData.offset;
+  ColorManipulation.rgbSequence = 4;
+  if(ColorManipulation.colorArrayEnd-54==0){
+    console.log('no color palette! manipulating pixels directly...');
+    ColorManipulation.colorArrayEnd = picData.fileSize;
+    ColorManipulation.rgbSequence = 3;
+  }
+  ColorManipulation.colorArray =buffer.slice(54,ColorManipulation.colorArrayEnd);
+  return ColorManipulation;
+};
+
+var colorInvert = function(buffer, picData) {
+  var colorData = ColorManipulation(buffer, picData);
+  for (var i=0; i<colorData.colorArrayEnd; i++) {
+    colorData.colorArray[i] = 255 - colorData.colorArray[i];
   }
   fs.writeFile(`${__dirname}/../assets/outputs/testing-invert.bmp`, buffer, (err) => {
     if(err) throw errorHandler(err);
   });
 };
 
-exports.greyScale = function (buffer, picData) {
-  var colorArrayEnd = picData.offset;
-  var colorArray = buffer.slice(54,colorArrayEnd);
-  for (var i=0; i<colorArray.length; i=i+4){
-    var rgba = colorArray.slice(i,i+4);
+var greyScale = function (buffer, picData) {
+  var colorData = ColorManipulation(buffer, picData);
+  for (var i=0; i<colorData.colorArray.length; i=i+colorData.rgbSequence){
+    var rgba = colorData.colorArray.slice(i,i+colorData.rgbSequence);
     var avgVal = (rgba[0] + rgba[1] + rgba[2])/ 3;
     rgba[0] = rgba[1] = rgba[2] = avgVal;
   }
@@ -27,18 +43,13 @@ exports.greyScale = function (buffer, picData) {
   });
 };
 
-exports.rgbScale = function (buffer, picData) {
-  var colorArrayEnd = picData.offset;
-  var colorArray = buffer.slice(54,colorArrayEnd);
-  for (var i=0; i<colorArray.length; i=i+4){
-    colorArray[i] = colorArray[i] * .5;
-    colorArray[i+1] = colorArray[i+1] * .5;
+var rgbScale = function (buffer, picData) {
+  var colorData = ColorManipulation(buffer, picData);
+  for (var i=0; i<colorData.colorArray.length; i=i+colorData.rgbSequence){
+    colorData.colorArray[i] = colorData.colorArray[i] * .5;
+    colorData.colorArray[i+1] = colorData.colorArray[i+1] * .5;
   }
   fs.writeFile(`${__dirname}/../assets/outputs/testingrgbscale.bmp`, buffer, (err) => {
     if(err) throw errorHandler(err);
   });
 };
-
-function errorHandler(err) {
-  console.log(`Aww dude!! ${err}`);
-}
