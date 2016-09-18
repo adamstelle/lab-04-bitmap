860a2724f47f20424ad3280698d11ab68e1dbcc9
JudyVue <judyvue87@gmail.com>
2016-09-18 10:40:27 -0700

    committing to switch branches

---
+++ judy-adam-lab/test/test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const bitMapHelper = require(`${__dirname}../lib/bitmap-file-helper`);
+
+
+describe('testing module bitMapHelper', function(){
+  describe('testing #colorInvert')
+})
