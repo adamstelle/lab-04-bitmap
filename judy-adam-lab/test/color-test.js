fc435ac281503dff3e2c125b7dcff61952e0c4a3
Adam Stelle <adamstelle@gmail.com>
2016-09-18 18:25:06 -0700

    Require chai

--- judy-adam-lab/test/color-test.js
+++ judy-adam-lab/test/color-test.js
@@ -1,25 +1,25 @@
 'use strict';
 
-const expect = require('chain').expect;
+const expect = require('chai').expect;
 const colorManipulator = require(`${__dirname}/../model/color-manipulator`);
 
-describe('Color Manipulator Module', function() {
-  describe('core ColorManipulation function', function() {
-    it('should access the color array within the bitmap buffer', function() {
-
-    });
-  })
-  describe('.colorInvert function', function() {
-    it('invert each color on the original buffer fil by subtracting it from 255', function() {
-
-    });
-    it('should write the modified buffer file to a new .bmp file', function() {
-
-    });
-  });
-  describe('.greyScale function', function(){
-    it('should access sets of 4 RGBA values in the buffer', function() {
-      
-    })
-  }))
-}
+// describe('Color Manipulator Module', function() {
+//   describe('core ColorManipulation function', function() {
+//     it('should access the color array within the bitmap buffer', function() {
+//
+//     });
+//   });
+//   describe('.colorInvert function', function() {
+//     it('invert each color on the original buffer fil by subtracting it from 255', function() {
+//
+//     });
+//     it('should write the modified buffer file to a new .bmp file', function() {
+//
+//     });
+//   });
+//   describe('.greyScale function', function(){
+//     it('should access sets of 4 RGBA values in the buffer', function() {
+//
+//     });
+//   });
+// });
