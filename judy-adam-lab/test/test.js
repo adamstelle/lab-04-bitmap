c9c408a8676b92904a419457ccae62f810a89290
JudyVue <judyvue87@gmail.com>
2016-09-18 18:22:07 -0700

    committing broken test to see if first arg is valid file path for adam's review

--- judy-adam-lab/test/test.js
+++ judy-adam-lab/test/test.js
@@ -1,8 +1,19 @@
 'use strict';
 
-const bitMapHelper = require(`${__dirname}../lib/bitmap-file-helper`);
+const fileHelper = require(`${__dirname}/../lib/bitmap-file-helper`);
+const colors = require(`${__dirname}/../model/color-constructor`);
+const assert = require('assert');
+const bitmapConstructor = require(`${__dirname}/../model/bitmap-constructor`);
+const EE = require('events');
+const ee = new EE();
 
 
-describe('testing module bitMapHelper', function(){
-  describe('testing #colorInvert')
-})
+describe('testing fileHelper module', function(){
+  describe('testing #readPicData', function(){
+    it('should show an error with bad filepath name as first arg', function(){
+      var result = fileHelper.readPicData('${__dirname}/..bad-directory', null,null);
+      //TODO: trying to test if bad directory was put as first arg, uncertain how to structure assert method to make the test pass, i.e., how to make error message strings match each other properly
+      assert.equal(result, 'Aww dude! Error: Error: ENOENT: no such file or directory, open '''${__dirname}/..bad-directory'', 'error msg not thrown'));
+    });
+  });
+});
